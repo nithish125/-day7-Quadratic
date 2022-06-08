81cd8f0871fb8d875092105466198488f85af2d8
nithish125 <nithishs3620@gmail.com>
2022-06-08 11:24:26 +0530

    update

---
+++ quadartic.js
@@ -0,0 +1,16 @@
+let a , b , c , d;
+
+a = 5;
+b = 10;
+c = 4;
+x=(b**2) - (4*a*c);
+y=Math.sqrt(x);
+z=2*a;
+quadarticequation1=((-b + y)/z);
+console.log(" positive squareroot value" +quadarticequation1 );
+quadarticequation1=((-b - y)/z);
+console.log(" negaitive squareroot value" +quadarticequation1 );
+
+
+
+
